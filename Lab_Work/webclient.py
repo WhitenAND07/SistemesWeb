b8bef523ee19b2fc275a2fa03923cd6cfe190dc3
WhitenAND07 <jordi14and@gmail.com>
2017-03-13 20:49:13 +0100

    Update webclient.py

--- Lab_Work/webclient.py
+++ Lab_Work/webclient.py
@@ -61,7 +61,7 @@ if __name__ == "__main__" :
 
     if not api_key:
         try:
-            api_key =  "27ccd3209edffffc" #sys.argv[1]
+            api_key =  sys.argv[1]
             wc = WeatherClient(api_key)
             result = wc.almanac("Lleida")
             print result
